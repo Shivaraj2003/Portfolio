e8b5a26b9744239bd48543fd7b02ffa86d3bbae3
Shivaraj2003 <shivarajshetty635@gmail.com>
2024-04-05 18:04:11 +0530

    Completed footer section

--- src/main.js
+++ src/main.js
@@ -9,5 +9,26 @@ const initApp = () =>{
     }
     hambergMenu.addEventListener('click',toggleMenu);
     mobileMenu.addEventListener('click',toggleMenu);
+
+    const project1 = document.getElementById('visit1');
+    project1.addEventListener('click',()=>{
+        document.getElementById('modal-id').style.display= 'flex';
+        document.getElementById('project-grid').style.display = 'none';
+    });
+    const close = document.getElementById('close');
+    const close1 = document.getElementById('close1');
+    close.addEventListener('click',()=>{
+        document.getElementById('modal-id').style.display= 'none';
+                document.getElementById('project-grid').style.display = 'grid';
+
+    });
+    close1.addEventListener('click',()=>{
+        document.getElementById('modal-id').style.display= 'none';
+                document.getElementById('project-grid').style.display = 'grid';
+
+    });
+
 }
+
+
 document.addEventListener('DOMContentLoaded',initApp);
